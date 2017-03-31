2cd735e460cd571f68c6c09c030215918116169d
Hysmun <throdlemassif@hotmail.com>
2017-03-31 11:34:33 +0200

    default

--- Step2/admin.c
+++ Step2/admin.c
@@ -62,24 +62,24 @@ void SaiSieRecord(struct Seance *UnRecord )
 
 void CreationAjoutFichier(char *NomFichier,struct Seance  *UnRecord )
 {
- FILE *sortie ;
- char Redo ;
- int nbr ;
+	FILE *sortie ;
+	//char Redo ;
+	//int nbr ;
 
- sortie = fopen(NomFichier,"a") ; /* Si le fichier existe, on le cree sinon on ajoute */
- if ( sortie == NULL )
-    {
-     fprintf(stderr,"Echec Ouverture\n") ;
-     exit(0) ;
-    }
- else
-    fprintf(stderr,"Ouverture reussie \n") ;
- setvbuf(sortie, (char *)NULL, _IOLBF, 0) ; /* ceci supprime la bufferisation */
- 
-  printf("Position actuelle dans le fichier %ld\n",ftell(sortie)) ;
-  nbr = fwrite(UnRecord,sizeof(struct Seance ),1,sortie) ;
-  fflush(sortie) ;
- fclose(sortie) ;
+	sortie = fopen(NomFichier,"a") ; /* Si le fichier existe, on le cree sinon on ajoute */
+	if ( sortie == NULL )
+	{
+		fprintf(stderr,"Echec Ouverture\n") ;
+		exit(0) ;
+	}
+	else
+		fprintf(stderr,"Ouverture reussie \n") ;
+	setvbuf(sortie, (char *)NULL, _IOLBF, 0) ; /* ceci supprime la bufferisation */
+
+	printf("Position actuelle dans le fichier %ld\n",ftell(sortie)) ;
+	fwrite(UnRecord,sizeof(struct Seance ),1,sortie) ;
+	fflush(sortie) ;
+	fclose(sortie) ;
 }
 
 void AfficheRecord(struct Seance  *UnRecord)
@@ -106,60 +106,60 @@ void AfficheFacture(struct Facture *UneFacture)
 
 void Listing(char *NomFichier)
 {
- struct Seance  UnRecord ;
- FILE *sortie ;
- char Tampon[80] ;
- int  Numero ;
- int  nbr ;
+	struct Seance  UnRecord ;
+	FILE *sortie ;
+	//char Tampon[80] ;
+	//int  Numero ;
+	int  nbr ;
 
- sortie = fopen(NomFichier,"r") ; /* Si le fichier existe, on le cree sinon on ajoute */
- if ( sortie == NULL )
-    {
-     fprintf(stderr,"Echec Ouverture\n") ;
-     exit(0) ;
-    }
- else
-    fprintf(stderr,"Ouverture reussie \n") ;
+	sortie = fopen(NomFichier,"r") ; /* Si le fichier existe, on le cree sinon on ajoute */
+	if ( sortie == NULL )
+	{
+		fprintf(stderr,"Echec Ouverture\n") ;
+		exit(0) ;
+	}
+	else
+		fprintf(stderr,"Ouverture reussie \n") ;
 
- 
- nbr = fread(&UnRecord,sizeof(UnRecord),1,sortie) ;
- 
- while ( !feof(sortie) )
- {
-  fprintf(stderr,"Record lu %d et Position actuelle dans le fichier %ld\n",nbr,ftell(sortie)) ;
-  AfficheRecord( &UnRecord) ;
-  nbr = fread(&UnRecord,sizeof(UnRecord),1,sortie) ;
- }
- fclose(sortie) ;
+
+	nbr = fread(&UnRecord,sizeof(UnRecord),1,sortie) ;
+
+	while ( !feof(sortie) )
+	{
+		fprintf(stderr,"Record lu %d et Position actuelle dans le fichier %ld\n",nbr,ftell(sortie)) ;
+		AfficheRecord( &UnRecord) ;
+		nbr = fread(&UnRecord,sizeof(UnRecord),1,sortie) ;
+	}
+	fclose(sortie) ;
 }
 
 void ListingFacture(char *NomFichier)
 {
- struct Facture UneFacture ;
- FILE *sortie ;
- char Tampon[80] ;
- int  Numero ;
- int  nbr ;
+	struct Facture UneFacture ;
+	FILE *sortie ;
+	//char Tampon[80] ;
+	//int  Numero ;
+	int  nbr ;
 
- sortie = fopen(NomFichier,"r") ; /* Si le fichier existe, on le cree sinon on ajoute */
- if ( sortie == NULL )
-    {
-     fprintf(stderr,"Echec Ouverture\n") ;
-     exit(0) ;
-    }
- else
-    fprintf(stderr,"Ouverture reussie \n") ;
+	sortie = fopen(NomFichier,"r") ; /* Si le fichier existe, on le cree sinon on ajoute */
+	if ( sortie == NULL )
+	{
+		fprintf(stderr,"Echec Ouverture\n") ;
+		exit(0) ;
+	}
+	else
+		fprintf(stderr,"Ouverture reussie \n") ;
 
- 
- nbr = fread(&UneFacture,sizeof(struct Facture),1,sortie) ;
- 
- while ( !feof(sortie) )
- {
-  fprintf(stderr,"Record lu %d et Position actuelle dans le fichier %ld\n",nbr,ftell(sortie)) ;
-  AfficheFacture( &UneFacture) ;
-  nbr = fread(&UneFacture,sizeof(struct Facture),1,sortie) ;
- }
- fclose(sortie) ;
+
+	nbr = fread(&UneFacture,sizeof(struct Facture),1,sortie) ;
+
+	while ( !feof(sortie) )
+	{
+		fprintf(stderr,"Record lu %d et Position actuelle dans le fichier %ld\n",nbr,ftell(sortie)) ;
+		AfficheFacture( &UneFacture) ;
+		nbr = fread(&UneFacture,sizeof(struct Facture),1,sortie) ;
+	}
+	fclose(sortie) ;
 }
 
 /****************************
@@ -172,60 +172,61 @@ void ListingFacture(char *NomFichier)
 
 int main()
 {
- char Choix ;
- char Tampon[80] ;
- int res ;
- struct Seance  UnRecord ;
- int Numero ;
- while(1)
- {
-  printf("----------------------\n") ;
-  printf("1) Ajout              \n") ;
-  printf("2) Seances              \n") ;  
-  printf("3) Recherche          \n") ;   
-  printf("4) Achat              \n") ;  
-  printf("5) Factures           \n") ;
-  printf("6) A propos           \n") ;
-  printf("7) exit               \n") ; 
-  printf("----------------------\n") ; 
-  Choix=ReadChar() ;
-  switch(Choix)
-  {
-   case '1': 
-             {
-              struct Seance  UnRecord ;
- 	     FILE *sortie ;
-             char Redo ;
-		
-             Redo='y' ;
- 	    while ( Redo=='Y' || Redo=='y')
- 	    {
-  		SaiSieRecord(&UnRecord ) ;
-  		CreationAjoutFichier("Seances",&UnRecord) ; 
-  		printf("Encoder un autre (Y/N) ?)") ;
-  		Redo=ReadChar() ;
- 	   }
- 
-	     break ;	
-             }
-   case '2': Listing("Seances") ;
-             break ;
-   case '3': printf("Saisie Reference:");
-   			 fflush(stdin);
-   			 scanf("%d",&Numero);
-   			 Recherche("Seances",Numero,&UnRecord);
-   			 AfficheRecord(&UnRecord);
-   			 break;
-   case '5': ListingFacture("Facture") ;
-	     break ;
-   case '6': APropos("Step 1","Brajkovic Antoine","Mauhin Remy") ;
-             break ;
-   
-	
-   case '7': exit(0) ; 
-  }
- }
- return 1;
+	char Choix ;
+	//char Tampon[80] ;
+	//int res ;
+	struct Seance  UnRecord ;
+	int Numero ;
+	while(1)
+	{
+		printf("----------------------\n") ;
+		printf("1) Ajout              \n") ;
+		printf("2) Seances              \n") ;  
+		printf("3) Recherche          \n") ;   
+		printf("4) Achat              \n") ;  
+		printf("5) Factures           \n") ;
+		printf("6) A propos           \n") ;
+		printf("7) exit               \n") ; 
+		printf("----------------------\n") ; 
+		Choix=ReadChar() ;
+		switch(Choix)
+		{
+			case '1': 
+			{
+				struct Seance  UnRecord ;
+				//FILE *sortie ;
+				char Redo ;
+
+				Redo='y' ;
+				while ( Redo=='Y' || Redo=='y')
+				{
+					SaiSieRecord(&UnRecord ) ;
+					CreationAjoutFichier("Seances",&UnRecord) ; 
+					printf("Encoder un autre (Y/N) ?)") ;
+					Redo=ReadChar() ;
+				}
+				break ;	
+			}
+			case '2': 
+				Listing("Seances") ;
+				break ;
+			case '3': printf("Saisie Reference:");
+				fflush(stdin);
+				scanf("%d",&Numero);
+				Recherche("Seances",Numero,&UnRecord);
+				AfficheRecord(&UnRecord);
+				break;
+			case '5': 
+				ListingFacture("Facture") ;
+				break ;
+			case '6': 
+				APropos("Step 1","Brajkovic Antoine","Mauhin Remy") ;
+				break ;
+			case '7': 
+				exit(0) ; 
+		}
+	}
+	return 1;
 }
 
 
--- Step2/cli.c
+++ Step2/cli.c
@@ -18,67 +18,68 @@ void die(char *s)
 
 int main(int argc, char *argv[])
 {
- int rc ;
- int Desc ;
- int tm ; 
- 
- u_long  IpSocket , IpServer;
- u_short PortSocket, PortServer ; 
- 
- struct sockaddr_in sthis ; /* this ce programme */
- struct sockaddr_in sos ; /* s = serveur */
- struct sockaddr_in sor ; /* r = remote */
- struct Requete UneRequete ;
+	int rc ;
+	int Desc ;
+	int tm ; 
 
- memset(&sthis,0,sizeof(struct sockaddr_in)) ;
- memset(&sos,0,sizeof(struct sockaddr_in)) ; 
- memset(&sor,0,sizeof(struct sockaddr_in)) ; 
- 
- if (argc!=5)
-  
-{
-    printf("cli client portc serveur ports\n") ;
-    exit(1) ;
- }
- 
- /* Récupération IP & port   */
- IpSocket= inet_addr(argv[1]);
- PortSocket = atoi(argv[2]);
- 
- IpServer = inet_addr(argv[3]) ;
- PortServer = atoi(argv[4]);
- 
- // Desc = CreateSockets(&psoo,&psos,,atoi(argv[2]),argv[3],atoi(argv[4])) ;
- Desc=creer_socket(SOCK_DGRAM,&IpSocket,PortSocket,&sthis);
- 
- if ( Desc == -1 )
-    die("CreateSockets:") ; 
- else
-    fprintf(stderr,"CreateSockets %d\n",Desc) ;
- 
-  sos.sin_family = AF_INET ;
-  sos.sin_addr.s_addr= IpServer ;
-  sos.sin_port = htons(PortServer) ;
+	u_long  IpSocket , IpServer;
+	u_short PortSocket, PortServer ; 
 
- 
- UneRequete.Type = Question ; 
- strncpy(UneRequete.Message , "Avec une structure: Bonjour" , sizeof(UneRequete.Message)) ;
- 
- rc = SendDatagram(Desc,&UneRequete,sizeof(struct Requete) ,&sos ) ;
+	struct sockaddr_in sthis ; /* this ce programme */
+	struct sockaddr_in sos ; /* s = serveur */
+	struct sockaddr_in sor ; /* r = remote */
+	struct Requete UneRequete ;
 
- if ( rc == -1 )
-    die("SendDatagram") ;
- else
-   fprintf(stderr,"Envoi de %d bytes\n",rc ) ;
- 
- memset(&UneRequete,0,sizeof(struct Requete)) ;
- tm = sizeof(struct Requete) ;
- 
-  rc = ReceiveDatagram( Desc, &UneRequete,tm, &sor ) ;
- if ( rc == -1 )
-    die("ReceiveDatagram") ;
- else
-   fprintf(stderr,"bytes recus:%d:%s\n",rc,UneRequete.Message ) ;
- 
- close(Desc) ;
+	memset(&sthis,0,sizeof(struct sockaddr_in)) ;
+	memset(&sos,0,sizeof(struct sockaddr_in)) ; 
+	memset(&sor,0,sizeof(struct sockaddr_in)) ; 
+
+	if (argc!=5)
+
+	{
+		printf("cli client portc serveur ports\n") ;
+		exit(1) ;
+	}
+
+	/* Récupération IP & port   */
+	IpSocket= inet_addr(argv[1]);
+	PortSocket = atoi(argv[2]);
+
+	IpServer = inet_addr(argv[3]) ;
+	PortServer = atoi(argv[4]);
+
+	// Desc = CreateSockets(&psoo,&psos,,atoi(argv[2]),argv[3],atoi(argv[4])) ;
+	Desc=creer_socket(SOCK_DGRAM,&IpSocket,PortSocket,&sthis);
+
+	if ( Desc == -1 )
+		die("CreateSockets:") ; 
+	else
+		fprintf(stderr,"CreateSockets %d\n",Desc) ;
+
+	sos.sin_family = AF_INET ;
+	sos.sin_addr.s_addr= IpServer ;
+	sos.sin_port = htons(PortServer) ;
+
+
+	UneRequete.Type = Question ; 
+	strncpy(UneRequete.Message , "Avec une structure: Bonjour" , sizeof(UneRequete.Message)) ;
+
+	rc = SendDatagram(Desc,&UneRequete,sizeof(struct Requete) ,&sos ) ;
+
+	if ( rc == -1 )
+		die("SendDatagram") ;
+	else
+		fprintf(stderr,"Envoi de %d bytes\n",rc ) ;
+
+	memset(&UneRequete,0,sizeof(struct Requete)) ;
+	tm = sizeof(struct Requete) ;
+
+	rc = ReceiveDatagram( Desc, &UneRequete,tm, &sor ) ;
+	if ( rc == -1 )
+		die("ReceiveDatagram") ;
+	else
+		fprintf(stderr,"bytes recus:%d:%s\n",rc,UneRequete.Message ) ;
+
+	close(Desc) ;
+	return 1;
 }
