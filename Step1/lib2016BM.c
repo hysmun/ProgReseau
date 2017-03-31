ecd3d505022a61c1ef6d00b17a9614373321f185
Hysmun <throdlemassif@hotmail.com>
2017-03-31 10:54:22 +0200

    default

--- Step1/lib2016BM.c
+++ Step1/lib2016BM.c
@@ -12,6 +12,28 @@ void APropos(char *Version,char *Nom1,char* Nom2)
 int Recherche(char* NomFichier, int Reference, Seance *UnRecord)
 {
 	//recherche d'un reccord dans le fichier 
+	int i, ret;
+	FILE *fp = fopen(NomFichier, "rb");
+	
+	if(fp == null)
+	{
+		printf("Erreur ouverture fichier");
+		UnRecord = null;
+		return -1;
+	}
+	printf("Ouverture reussie");
+	fseek(fp, 0, SEEK_SET);
+	
+	ret = fread(UnRecord, sizeof(Seance), 1, fp);
+	for(i=0; ret != 0; i++)
+	{
+		if(UnRecord->Reference == Reference)
+		{
+			printf("trouver");
+			return 1;
+		}				
+	}
+	
 }
 
 
