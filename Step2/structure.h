48a583676f81a6e15290bd89ee42217d874a9e16
Hysmun <throdlemassif@hotmail.com>
2017-03-31 11:41:35 +0200

    default

--- Step2/structure.h
+++ Step2/structure.h
@@ -1,11 +1,23 @@
 
-enum TypeRequete {
-       Question = 1 ,
-       Reponse  = 2 
-     } ;
-
-struct Requete
+typedef enum TypeRequete 
 {
-  enum TypeRequete Type ;
-  char Message[40] ; 
-} ; 
+   Question = 1 ,
+	Achat = 2 ,
+	Livraison= 3 ,
+	OK = 4,
+	Fail = 5
+}TypeRequete;
+
+typedef struct Requete
+{
+	enum TypeRequete Type ;
+	int Numero ;
+	int NumeroFacture ;
+	int Date ;
+	int Reference ;
+	int Places ;
+	int Prix ;
+	char Film[80] ;
+	char NomClient[80] ;
+	char Realisateur[30];
+}Requete;
