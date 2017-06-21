a75b38cc4bcc9e1f1d259ca329d050d4275aeda8
Hysmun <throdlemassif@hotmail.com>
2017-06-21 20:52:00 +0200

    creation step5a

---
+++ Step5B/cli.c
@@ -0,0 +1,142 @@
+/*---------------------------------------------------------------
+   Vanstapel Herman
+   ex02\cli.c
+
+ Le client dit bonjour en utilisant un structure  et
+ le serveur fait de même
+------------------------------------------------------------------*/
+#include <stdio.h>
+#include <string.h>
+#include "../udplib/udplib.h"
+#include "structure.h"
+#include "utils.h"
+
+void die(char *s)
+{
+    perror(s);
+    exit(1);
+}
+
+int main(int argc, char *argv[])
+{
+	int rc ;
+	int Desc ;
+	int tm ;
+	int choix = '0';
+	int i=0;
+	char ctmp[255];
+	u_long  IpSocket , IpServer;
+	u_short PortSocket, PortServer ; 
+
+	struct sockaddr_in sthis ; /* this ce programme */
+	struct sockaddr_in sos ; /* s = serveur */
+	struct sockaddr_in sor ; /* r = remote */
+	struct Requete UneRequete ;
+	
+	
+
+	memset(&sthis,0,sizeof(struct sockaddr_in)) ;
+	memset(&sos,0,sizeof(struct sockaddr_in)) ; 
+	memset(&sor,0,sizeof(struct sockaddr_in)) ; 
+
+	if (argc!=5)
+
+	{
+		printf("cli client portc serveur ports\n") ;
+		exit(1) ;
+	}
+
+	/* Récupération IP & port   */
+	IpSocket= inet_addr(argv[1]);
+	PortSocket = atoi(argv[2]);
+
+	IpServer = inet_addr(argv[3]) ;
+	PortServer = atoi(argv[4]);
+
+	// Desc = CreateSockets(&psoo,&psos,,atoi(argv[2]),argv[3],atoi(argv[4])) ;
+	Desc=creer_socket(SOCK_DGRAM,&IpSocket,PortSocket,&sthis);
+
+	if ( Desc == -1 )
+		die("CreateSockets:") ; 
+	else
+		fprintf(stderr,"CreateSockets %d\n",Desc) ;
+
+	sos.sin_family = AF_INET ;
+	sos.sin_addr.s_addr= IpServer ;
+	sos.sin_port = htons(PortServer) ;
+	/* Début partie modifée */
+
+	while(choix != 'q')
+	{
+		printf("----------------------    \n") ;
+		printf("1) Demander une reference\n");
+		printf("2) Acheter une reference\n");
+		printf("6) A propos           \n") ;
+		printf("7) Exit               \n") ; 
+		printf("----------------------\n") ;
+		choix=ReadChar();
+		switch(choix)
+		{
+			case '1':
+				UneRequete.Type = Question;
+				printf("Saisie numero de seance : ");
+				fgets(ctmp,sizeof ctmp,stdin );
+				choix = atoi(ctmp);
+				UneRequete.Reference = choix;
+				break;
+			case '2':
+				UneRequete.Type = Achat;
+				printf("Nom Client : ");
+				fgets(UneRequete.NomClient,sizeof(UneRequete.NomClient),stdin);
+				printf("Reference : ");
+				fgets(ctmp,sizeof(ctmp),stdin);
+				UneRequete.Reference = atoi(ctmp);
+				printf("Places : ");
+				fgets(ctmp,sizeof(ctmp),stdin);
+				UneRequete.Places = atoi(ctmp);
+				UneRequete.Date = 0;
+				break;
+			case '6':
+				break;
+			case '7':
+				choix = 'q';
+				break;
+		}
+		
+		/* Fin partie modifée*/
+		if(choix != 'q')
+		{
+			rc = SendDatagram(Desc,&UneRequete,sizeof(struct Requete) ,&sos ) ;
+
+			if ( rc == -1 )
+				die("SendDatagram") ;
+			else
+				fprintf(stderr,"Envoi de %d bytes\n",rc ) ;
+
+			memset(&UneRequete,0,sizeof(struct Requete)) ;
+			tm = sizeof(struct Requete) ;
+
+			rc = ReceiveDatagram( Desc, &UneRequete,tm, &sor ) ;
+			if ( rc == -1 )
+				die("ReceiveDatagram") ;
+			else
+			{
+				fprintf(stderr,"bytes recus:%d\n",rc) ;
+				switch((int)UneRequete.Type)
+				{
+					case OK:
+						fprintf(stderr,"Film:%s\t\tRealisateur:%s\t\tPlaces:%d\n",UneRequete.Film,UneRequete.Realisateur,UneRequete.Places);
+						break;
+					case Fail:
+						printf("\n\n ERROR fail serv\n\n");
+						break;
+				}
+			}
+		}
+		i++;
+		UneRequete.Type = Fail;
+	}
+
+	close(Desc) ;
+	return 1;
+}
---
+++ Step5B/ser.c
@@ -0,0 +1,136 @@
+/*--------------------------------------
+  Herman Vanstapel
+  
+  ex02\ser.c 
+  
+  Un serveur recevant une structure 
+----------------------------------------*/
+
+#include <stdio.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#include "../udplib/udplib.h"
+#include "structure.h"
+#include "data.h"
+#include "utils.h"
+#include "lib2016BM.h"
+
+void die(char *s);
+
+int main(int argc,char *argv[])
+{
+	int rc ;
+	int Desc ;
+	int res;
+	int send = 0;
+	char ctmp[255];
+	struct sockaddr_in sthis ; /* this ce programme */
+	struct sockaddr_in sos ; /* s = serveur */
+	struct sockaddr_in sor ; /* r = remote */
+
+	u_long  IpSocket ;
+	u_short PortSocket ;
+
+	int tm ;
+	struct Requete UneRequete ;
+	struct Seance UneSeance;
+
+	memset(&sthis,0,sizeof(struct sockaddr_in)) ;
+	memset(&sos,0,sizeof(struct sockaddr_in)) ; 
+	memset(&sor,0,sizeof(struct sockaddr_in)) ; 
+
+	printf("Ceci est le serveur\n") ;
+	if ( argc!=3)
+	{
+		printf("ser ser port cli\n") ;
+		exit(1) ;
+	}
+
+	/* Récupération IP & port   */
+	IpSocket= inet_addr(argv[1]);
+	PortSocket = atoi(argv[2]);
+	// Desc = CreateSockets(&psoo,&psos,,atoi(argv[2]),argv[3],atoi(argv[4])) ;
+	Desc=creer_socket(SOCK_DGRAM,&IpSocket,PortSocket,&sthis);
+
+	if ( Desc == -1 )
+		die("CreateSockets:") ;
+	else
+		fprintf(stderr,"CreateSockets %d\n",Desc) ;
+	while(1)
+	{
+		tm = sizeof(struct Requete) ;
+		rc = ReceiveDatagram( Desc,&UneRequete ,tm, &sor ) ;
+		if ( rc == -1 )
+			die("ReceiveDatagram") ;
+		else
+			fprintf(stderr,"bytes recus:%d Reference :%d\n",rc,UneRequete.Reference ) ;
+		
+		printf("Received packed from %s\n",ipString(sor, ctmp));
+		send = 0;
+		switch((int)UneRequete.Type)
+		{
+			case Question:
+				printf("Type recu %d\n", UneRequete.Type) ;
+				res = Recherche("Seances",UneRequete.Reference,&UneSeance);
+				if(res != -1)
+					fprintf(stderr,"res:%d -- Reference:%s -- Realisateur:%s -- Quantite :%d\n",res,UneSeance.Film,UneSeance.Realisateur,UneSeance.Places);
+				else
+				{
+					UneRequete.Type = Fail ;
+					send = 1;
+					break;
+				}
+				UneRequete.Type = OK ;
+				strcpy(UneRequete.Film,UneSeance.Film);
+				strcpy(UneRequete.Realisateur,UneSeance.Realisateur);
+				UneRequete.Places = UneSeance.Places;
+				send = 1;
+				break;
+			case Achat:
+				if(Reservation("Seances", UneRequete.Reference, UneRequete.Places) < 1)
+				{
+					printf("Erreur reservation !!!\n");
+					break;
+				}
+				UneRequete.NumeroFacture = Facturation("Factures", UneRequete.NomClient, UneRequete.Date, UneRequete.Places, UneRequete.Reference);
+				
+				send = 1;
+				break;
+			case Livraison:
+				break;
+			case Fail:
+				UneRequete.Type = Fail ;
+				printf("\n\n ERROR envois de FAIL \n\n");
+				send = 1;
+				break;
+		}
+		
+		/* attention l'enum peut être codé en short */
+		/* reponse avec psos */
+
+		/* Début partie modifiée*/
+		
+		
+		
+		/* Fin partie modifiée*/
+		if(send == 1)
+		{
+			rc = SendDatagram(Desc,&UneRequete,sizeof(struct Requete) ,&sor ) ;
+			if ( rc == -1 )
+				die("SendDatagram:") ;
+			else
+				fprintf(stderr,"bytes envoyes:%d\n",rc ) ;
+		}
+
+	}
+
+	close(Desc) ;
+	return 1;
+}
+
+void die(char *s)
+{
+    perror(s);
+    exit(1);
+}
