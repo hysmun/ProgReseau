5caa24604236c53120cd21e1c5c6f6bc9da4db31
Hysmun <throdlemassif@hotmail.com>
2017-06-22 11:59:45 +0200

    truc

---
+++ Step5B/data.h
@@ -0,0 +1,22 @@
+#ifndef DATAH
+#define DATAH
+
+typedef struct Seance 
+{ 
+	int Reference ;
+	char Film[80] ;
+	int  Places ;
+	int  Salle ;
+	char Date[10] ;
+	char Realisateur[30];
+}Seance;
+
+typedef struct Facture
+{
+	int NumeroFacturation ;
+	char NomClient[60] ;
+	int DateFacturation ;
+	int Places ;
+	int Reference ;
+}Facture;  	
+#endif
